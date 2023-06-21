f942bffa7d1c615fd9c1a7d3e2736661b516226f
Sasha Sinitsyn <Sannitsa@yahoo.com>
2023-06-22 01:23:28 +0300

    fix: basic server description added

--- server-runner.py
+++ server-runner.py
@@ -7,7 +7,13 @@ PORT = 9000
 Handler = http.server.SimpleHTTPRequestHandler
 Handler.extensions_map.update({
       ".js": "application/javascript",
-});
+})
+
+print('=' * 32)
+print('Running on http://localhost:9000')
+print('=' * 32)
+print('This server is going to be just a proxy to your local files. Ex.: a request to "./ad/algo.js"', \
+    'will be interpreted as "localhost:9000/ad/algo.js"')
 
 httpd = socketserver.TCPServer(("", PORT), Handler)
 httpd.serve_forever()
